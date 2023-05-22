5c16f6b2258f645acd5e9ef8bc837f51a0adb722
Borovaneca <petyoveselinov@gmail.com>
2023-05-22 15:42:55 +0300

    Update

---
+++ Data Definition and Datatypes/Exercise/0.CreateDatabase.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `minions` (
+`id` INT auto_increment primary key,
+`name` varchar(255),
+`age` INT
+);
+CREATE TABLE `towns` (
+`town_id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(255) NOT NULL
+);
