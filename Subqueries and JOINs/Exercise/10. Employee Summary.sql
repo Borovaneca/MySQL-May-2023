89868bb7530a939448f18a0eae59c4264fc1a22a
Petyo Veselinov <borovaneca@virgilio.it>
2023-05-31 01:13:43 +0300

    Create 10. Employee Summary.sql

---
+++ Subqueries and JOINs/Exercise/10. Employee Summary.sql
@@ -0,0 +1,9 @@
+SELECT e.employee_id,
+       CONCAT(e.`first_name`, ' ', e.`last_name`) AS `employee_name`,
+       CONCAT(m.`first_name`, ' ', m.`last_name`) AS `manager_name`,
+       d.`name`                                   AS `department_name`
+FROM employees AS e
+         JOIN employees AS m ON e.manager_id = m.employee_id
+         JOIN departments d on e.department_id = d.department_id
+ORDER BY e.employee_id
+LIMIT 5;
